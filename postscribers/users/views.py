cbde95007b6354246acac3a234252925c9427e9e
Efrat Tamirat <moobinhood45678@gmail.com>
2024-04-21 18:45:43 +0300

    First Django commit

---
+++ postscribers/users/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+
+# Create your views here.
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users-login')
+    else:
+        form = SignUpForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/sign_up.html', context)
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST or None, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST or None, request.FILES or None, instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profilemodel)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'users/profile.html', context)
+
+'''
+@login_required
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'  # Specify the template to display after logout
+
+    def get_next_page(self):
+        next_page = super().get_next_page('users/logout.html')
+        # Perform any additional actions or customizations
+        # before redirecting to the next page after logout
+        return next_page
+'''
+
+
+
+
+
+
+
+
+
+
